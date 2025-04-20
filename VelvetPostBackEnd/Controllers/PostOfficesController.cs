3755ebd94d28fb6cbdaf14f8321700ceaffef69b
Володимир Лев <next.ven@gmail.com>
2025-04-21 02:22:48 +0300

    Finished PostOffices controller

--- VelvetPostBackEnd/Controllers/PostOfficesController.cs
+++ VelvetPostBackEnd/Controllers/PostOfficesController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using VelvetPostBackEnd.Data;
+using VelvetPostBackEnd.DTOs;
 using VelvetPostBackEnd.Models;
 
 namespace VelvetPostBackEnd.Controllers;
@@ -43,5 +44,77 @@ public class PostOfficesController : ControllerBase
 
     [HttpPut("{id}")]
     [Authorize(Roles = "Admin")]
-    public async Task<IActionResult> UpdatePostOffice(int id, [FromBody] PostOffice Up)
+    public async Task<IActionResult> UpdatePostOffice(int id, [FromBody] PostOfficeDTO UpdatedPostOffice)
+    {
+        var existingPostOffice = await _context.PostOffices.FindAsync(id);
+        if (existingPostOffice == null)
+        {
+            return NotFound("Відділення не знайдено");
+        }
+
+        existingPostOffice.Name = UpdatedPostOffice.Name;
+        existingPostOffice.Address = UpdatedPostOffice.Address;
+        existingPostOffice.City = UpdatedPostOffice.City;
+        existingPostOffice.PhoneNumber = UpdatedPostOffice.PhoneNumber;
+        existingPostOffice.TerminalId = UpdatedPostOffice.TerminalId;
+
+        try
+        {
+            await _context.SaveChangesAsync();
+            return Ok(existingPostOffice);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, $"Помилка при оновленні: {ex.Message}");
+        }
+    }
+
+    [HttpDelete("{id}")]
+    [Authorize(Roles = "Admin")]
+    public async Task<IActionResult> DeletePostOffice(int id)
+    {
+        var postOffice = await _context.PostOffices.FindAsync(id);
+        if (postOffice == null)
+        {
+            return NotFound("Відділення не знайдено");
+        }
+
+        try
+        {
+            _context.PostOffices.Remove(postOffice);
+            await _context.SaveChangesAsync();
+            return Ok($"Відділення з ID {id} успішно видалено");
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, $"Помилка при видаленні: {ex.Message}");
+        }
+    }
+
+    [HttpPost("create")]
+    [Authorize(Roles = "Admin")]
+    public async Task<IActionResult> CreatePostOffice(PostOfficeDTO postOffice)
+    {
+        if (ModelState.IsValid)
+        {
+            if (postOffice.TerminalId >= 0 && _context.Terminals.Any(t => t.Id == postOffice.TerminalId))
+            {
+                var po = new PostOffice
+                {
+                    TerminalId = postOffice.TerminalId,
+                    Name = postOffice.Name,
+                    Address = postOffice.Address,
+                    City = postOffice.City,
+                    PhoneNumber = postOffice.PhoneNumber,
+                };
+
+                _context.PostOffices.Add(po);
+                _context.SaveChangesAsync();
+                return Ok($"Відділення {po.Name} успішно створено");
+            }
+
+            return BadRequest("Терміналу з заданим ID не існує");
+        }
+        return BadRequest("Спробуйте пізніше");
+    }
 }
---
+++ VelvetPostBackEnd/DTOs/PostOfficeDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace VelvetPostBackEnd.DTOs;
+
+
+public class PostOfficeDTO
+{
+    public string Name {  get; set; }
+    public string Address {  get; set; }
+    public string City { get; set; }
+    public string PhoneNumber { get; set; }
+    public int TerminalId { get; set; }
+}
